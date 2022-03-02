f6a8fb5145656c726c5e022fe902503491673e1c
Krisk <31748409+BubblyGumKrisk@users.noreply.github.com>
2022-03-02 15:48:36 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,90 @@
 import sys
 import pygame
-from pygame import K_w, K_a, K_s, K_d
+from pygame import K_w, K_a, K_s, K_d, K_SPACE
+from random import randint
 pygame.init()
 
-fps = 60
+fps = 8
 clock = pygame.time.Clock()
 width, height = 256, 160
 true_screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
 screen = pygame.Surface((width, height))
 tile_size = 16
 
+class Enemy(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		super().__init__()
+		self.x = x
+		self.y = y
+		self.image = pygame.Surface((tile_size, tile_size))
+		self.wepimg = pygame.Surface((tile_size, tile_size))
+		self.rect = pygame.Rect(x*tile_size, y*tile_size, tile_size, tile_size)
+		
+	def update(self):
+		self.x += randint(-1,1)
+		
+		
+		
+		self.rect.x = self.x*16
+		self.rect.y = self.y*16
+
 class Player:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.image = pygame.Surface((tile_size, tile_size))
-        self.rect = pygame.Rect(x*tile_size, y*tile_size, tile_size, tile_size)
-        self.walk_cooldown_max = 10
-        self.walk_cooldown = 0
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+		self.image = pygame.Surface((tile_size, tile_size))
+		self.wepimg = pygame.Surface((tile_size, tile_size))
+		self.rect = pygame.Rect(x*tile_size, y*tile_size, tile_size, tile_size)
+		self.dir = [0, 0]
+		self.insafeplace = False
 
-    def draw(self, s):
-        s.blit(self.image, (self.rect.x, self.rect.y))
+	def draw(self):
+		screen.blit(self.image, (self.rect.x, self.rect.y))
 
-    def update(self):
-        self.walk_cooldown -= 1
-        keys = pygame.key.get_pressed()
-        if self.walk_cooldown <= 0:
-            if keys[K_w]:
-                self.rect.y -= tile_size
-                self.walk_cooldown = self.walk_cooldown_max
-            elif keys[K_s]:
-                self.rect.y += tile_size
-                self.walk_cooldown = self.walk_cooldown_max
-            elif keys[K_a]:
-                self.rect.x -= tile_size
-                self.walk_cooldown = self.walk_cooldown_max
-            elif keys[K_d]:
-                self.rect.x += tile_size
-                self.walk_cooldown = self.walk_cooldown_max
+	def update(self):
+		keys = pygame.key.get_pressed()
+		if keys[K_SPACE] and not self.insafeplace:
+			screen.blit(self.wepimg, ((self.x+self.dir[0])*16, (self.y+self.dir[1])*16))
+		elif keys[K_w]:
+			self.dir = [0, -1]
+			self.y -= 1
+		elif keys[K_s]:
+			self.dir = [0, 1]
+			self.y += 1
+		elif keys[K_a]:
+			self.dir = [-1, 0]
+			self.x -= 1
+		elif keys[K_d]:
+			self.dir = [1, 0]
+			self.x += 1
+				
+				
+		self.rect.y = self.y*16
+		self.rect.x = self.x*16
 
 def main():
-    player = Player(15//2, 9//2)
-    while True:
-        screen.fill("#FFFFFF")
+	player = Player(15//2, 9//2)
+	
+	enim_group = pygame.sprite.Group()
+	enim_group.add(Enemy(1, 1))
+	while True:
+		screen.fill("#FFFFFF")
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
 
-        player.update()
-        player.draw(screen)
+		enim_group.update()
+		enim_group.draw(screen)	
+				
+		player.update()
+		player.draw()
+		
 
-        true_screen.blit(pygame.transform.scale(screen, true_screen.get_rect().size), (0, 0))
-        pygame.display.flip()
-        clock.tick(fps)
+		true_screen.blit(pygame.transform.scale(screen, true_screen.get_rect().size), (0, 0))
+		pygame.display.flip()
+		clock.tick(fps)
 
 if __name__ == "__main__":
-    main()
+	main()
