559e31696a70d9679e4ee6412ce664c1a0639294
Krisk <31748409+BubblyGumKrisk@users.noreply.github.com>
2022-04-21 09:05:02 -0700

    Update leveleditor.py

--- leveleditor.py
+++ leveleditor.py
@@ -1,5 +1,6 @@
 import sys, pygame
 from math import floor
+import level
 
 pygame.init()
 
@@ -14,18 +15,43 @@ SCREEN = pygame.Surface((WIDTH, HEIGHT))
 BLACK = "#000000"
 WHITE = "#FFFFFF"
 
+def getmousepos():
+	x, y = pygame.mouse.get_pos()
+	w, h = TRUE_SCREEN.get_size()
+	return (x/(w/WIDTH), y/(h/HEIGHT))
 
 class Tile(pygame.sprite.Sprite):
-	def __init__(self, x, y):
+	def __init__(self, x, y, mode):
 		super().__init__()
 		self.image = pygame.Surface((TDIMS, TDIMS))
-		self.image.fill(WHITE)
 		self.rect = pygame.Rect((x, y), self.image.get_size())
-
-
+		self.type = mode
+		# 0 is normal
+		# 1 is kill
+		# 2 is moving
+		self.col = (WHITE if self.type == 0 else "#FF0000")
+		self.image.fill(self.col)
+		
 def main():
 	tilegroup = pygame.sprite.Group()
-
+	mode = 1
+	startpos = (0, 0)
+	try:
+		x, y = 0, 0
+		for r in eval(f"level.{sys.argv[2]}"):
+			for t in r:
+				if t == 1:
+					tilegroup.add(Tile(x, y, 0))
+				if t == 3:
+					tilegroup.add(Tile(x, y, 1))
+				if t == 2:
+					startpos = (x, y)
+				x += 32
+			y += 32
+			x = 0
+	except:
+		pass
+	
 	while True:
 		SCREEN.fill(BLACK)
 		for event in pygame.event.get():
@@ -35,14 +61,13 @@ def main():
 					toadd = []
 					for x in range(0, WIDTH, 32):
 						added = False
-						print(x)
 						for t in tilegroup.sprites():
 							if t.rect.x == x and t.rect.y == y:
-								toadd.append(1)
+								toadd.append(1 if t.type == 0 else (3 if t.type == 1 else 4))
 								added = True
 								break
 						if not added:
-							toadd.append(0)
+							toadd.append(0 if ((x, y) != startpos) else 2)
 					matrix.append(toadd)
 				
 				with open("level.py", 'a') as f:
@@ -53,29 +78,45 @@ def main():
 				
 				pygame.quit()
 				sys.exit()
+		
+		keys = pygame.key.get_pressed()
+		if keys[pygame.K_0]:
+			mode = 0
+		if keys[pygame.K_1]:
+			mode = 1
+		if keys[pygame.K_2]:
+			mode = 2
+		if keys[pygame.K_3]:
+			mode = 3
 
 		if pygame.mouse.get_pressed()[0]:
-			x, y = pygame.mouse.get_pos()
+			x, y = getmousepos()
 			x = floor(x/32)*32
 			y = floor(y/32)*32
-			shouldadd = True
-			for t in tilegroup.sprites():
-				if t.rect.x == x and t.rect.y == y:
-					shouldadd = False
-					break
-			if shouldadd:
-				tilegroup.add(Tile(x, y))
-		if pygame.mouse.get_pressed()[2]:
-			x, y = pygame.mouse.get_pos()
-			x = floor(x/32)*32
-			y = floor(y/32)*32
-			for t in tilegroup.sprites():
-				if t.rect.x == x and t.rect.y == y:
-					tilegroup.remove(t)
-					break
+			if mode == 1 or mode == 3:
+				shouldadd = True
+				for t in tilegroup.sprites():
+					if t.rect.x == x and t.rect.y == y:
+						shouldadd = False
+						break
+				if shouldadd:
+					tilegroup.add(Tile(x, y, (0 if mode==1 else 1)))
+			if mode == 0:
+				for t in tilegroup.sprites():
+					if t.rect.x == x and t.rect.y == y:
+						tilegroup.remove(t)
+						break
+			if mode == 2:
+				startpos = (x, y)
+						
+		pos = getmousepos()
+						
 				
 		tilegroup.update()
-
+		
+		toblit = pygame.Surface((TDIMS, TDIMS))
+		toblit.fill("#aaaaaa" if mode == 1 else ("#88444444" if mode == 0 else ("#ddb500" if mode == 2 else "#ff0000")))
+		SCREEN.blit(toblit, (floor(pos[0]/32)*32, floor(pos[1]/32)*32))
 		tilegroup.draw(SCREEN)
 
 		TRUE_SCREEN.blit(pygame.transform.scale(SCREEN, TRUE_SCREEN.get_size()), (0, 0))
