ef5e40b66b1f9c3adfcebc577f861aff2a8ce5c5
Krisk <31748409+BubblyGumKrisk@users.noreply.github.com>
2022-04-21 09:05:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
-from level import start
+import level
+from random import randint as random
 
 pygame.init()
 
@@ -16,19 +17,42 @@ SCREEN = pygame.Surface((WIDTH, HEIGHT))
 BLACK = "#000000"
 WHITE = "#FFFFFF"
 
+# returns startpos
+def loadlevel(level, tilegroup):
+	tilegroup.empty()
+	x, y = 0, 0
+	startpos = (0, 0)
+	for r in level:
+		for t in r:
+			if t == 1:
+				tilegroup.add(Tile(x, y, 0))
+			if t == 3:
+				tilegroup.add(Tile(x, y, 1))
+			if t == 2:
+				startpos = (x, y)
+			x += 32
+		y += 32
+		x = 0
+	return startpos
+
 
 class Tile(pygame.sprite.Sprite):
-	def __init__(self, x, y):
+	def __init__(self, x, y, mode):
 		super().__init__()
 		self.image = pygame.Surface((TDIMS, TDIMS))
-		self.image.fill(WHITE)
 		self.rect = pygame.Rect((x, y), self.image.get_size())
+		self.type = mode
+		# 0 is normal
+		# 1 is kill
+		# 2 is moving
+		self.col = (WHITE if self.type != 1 else BLACK)
+		self.image.fill(self.col)
 
 		
 class Player:
-	def __init__(self):
+	def __init__(self, pos):
 		self.image = pygame.Surface((TDIMS, TDIMS))
-		self.rect = pygame.Rect((WIDTH/2, 0), self.image.get_size())
+		self.rect = pygame.Rect(pos, self.image.get_size())
 		self.image.fill(WHITE)
 
 		self.xacel = 0
@@ -37,8 +61,12 @@ class Player:
 		self.onground = False
 		self.speed = 1
 		self.jump = 16
+		self.offscreen = "no"  # can be no, up, dw, lf, rt
+							   # used to change level
+		self.dead = False
 
 	def update(self, tiles):
+		# input
 		keys = pygame.key.get_pressed()
 
 		if keys[pygame.K_a]:
@@ -50,54 +78,77 @@ class Player:
 			self.onground = False
 			self.rect.bottom -= 1
 
+		# collision
 		self.onground = False
 		for t in tiles:
 			if self.rect.colliderect(t.rect):
+				if t.type == 1:
+					self.dead = True
+					break
 				if self.rect.y >= t.rect.y:
-					self.yacel = -GRAVITY
 					self.onground = False
+					self.yacel = -GRAVITY
 				if self.rect.bottom > t.rect.top > self.rect.bottom - TDIMS:
 					self.yacel = 0
 					self.rect.bottom = t.rect.top + 1
 					self.onground = True
 				else:
-					if keys[pygame.K_d] or self.rect.centerx < t.rect.centerx:
+					counter = 0
+					if self.rect.centerx < t.rect.centerx:
 						while self.rect.colliderect(t.rect):
 							self.rect.x -= 1
-						self.rect.x -= 1
-						self.xacel *= -1
-					if keys[pygame.K_a] or self.rect.centerx > t.rect.centerx:
+							counter += 1
+						if counter >= TDIMS/2:
+							self.rect.x += counter
+						else:
+							self.rect.x -= 1
+							self.xacel *= -1.1
+					if self.rect.centerx > t.rect.centerx:
 						while self.rect.colliderect(t.rect):
 							self.rect.x += 1
-						self.rect.x += 1
-						self.xacel *= -1
-
+							counter += 1
+						if counter >= TDIMS/2:
+							self.rect.x -= counter
+						else:
+							self.rect.x += 1
+							self.xacel *= -1.1
+		
+		# applying gravity and friction
 		if self.onground:
 			self.yacel = 0
 		else:
 			self.yacel -= GRAVITY
-
 		self.xacel *= self.friction
+		# applying acels to rect
 		self.rect.x += round(self.xacel)
 		self.rect.y += round(self.yacel)
+		
+		# checking if offscreen
+		if self.rect.left < 0:
+			self.offscreen = "lf"
+		elif self.rect.right >= WIDTH:
+			self.offscreen = "rt"
+		elif self.rect.top < 0:
+			self.offscreen = "up"
+		elif self.rect.bottom >= HEIGHT:
+			self.offscreen = "dw"
+		else:
+			self.offscreen = "no"
+		self.rect.clamp_ip(SCREEN.get_rect())
 
 	def draw(self):
 		SCREEN.blit(self.image, self.rect)
 
 
 def main():
-	player = Player()
 
 	tilegroup = pygame.sprite.Group()
 
-	x, y = 0, 0
-	for r in start:
-		for t in r:
-			if t == 1:
-				tilegroup.add(Tile(x, y))
-			x += 32
-		y += 32
-		x = 0
+	startpos = loadlevel(level.start, tilegroup)
+		
+	player = Player(startpos)
+	
+	lastdir = "dw"
 
 	while True:
 		SCREEN.fill(BLACK)
@@ -107,8 +158,21 @@ def main():
 				sys.exit()
 
 		player.update(tilegroup.sprites())
-		#tilegroup.update()
-
+		if player.dead:
+			main()
+			return 0
+		if player.offscreen != "no" and ((player.offscreen == "up" and lastdir != "dw") or (player.offscreen == "lf" and lastdir != "rt") or (player.offscreen == "rt" and lastdir != "lf") or (player.offscreen == "dw" and lastdir != "up")):
+			while True:
+				try:
+					startpos = loadlevel(eval(f"level.{player.offscreen}{random(0, 20)}"), tilegroup)
+					player.rect.x, player.rect.y = startpos
+					lastdir = player.offscreen
+					if player.offscreen == "up":
+						player.yacel = -player.jump
+					break
+				except AttributeError:
+					continue
+				
 		player.draw()
 		tilegroup.draw(SCREEN)
 
